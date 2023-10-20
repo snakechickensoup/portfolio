935dcc1aacc8ab274839a56d34341ed2201eec42
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2023-10-20 15:47:05 +0900

    modify tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      default: '#2A292A',
+      white: '#F5F5F5',
+      green: '#31854D',
+    },
+    fontSize: {
+      default: ['1rem', '1.5rem'] /* 16px */,
+      lg: ['1.875rem', '2.25rem'] /* 32px*/,
+    },
     extend: {},
   },
   plugins: [],
