4e165fdf89f79259812ff8156792f60b5c053fdc
snakechickensoup <141708304+snakechickensoup@users.noreply.github.com>
2023-10-25 17:16:31 +0900

    feat header hamburger menu

--- src/shared/components/layout/header/Header.tsx
+++ src/shared/components/layout/header/Header.tsx
@@ -1,14 +1,12 @@
-import MenuIcon from 'assets/icons/Menu.svg?react';
 import ArrowIcon from 'assets/icons/Arrow.svg?react';
 import Button from 'shared/components/button/Button';
 import { BUTTON_TYPES } from 'shared/components/button/constants';
+import Menu from './Menu';
 
 const Header = () => {
   return (
     <header className='relative flex items-center gap-2 border-b'>
-      <Button handleClick={() => {}} styles={BUTTON_TYPES.menu} title='Menu'>
-        <MenuIcon />
-      </Button>
+      <Menu />
       <h1 className='text-lg'>강미정</h1>
       <Button
         handleClick={() => {}}
